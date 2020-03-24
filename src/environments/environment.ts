39f4fd72a6212d33a32b285bd9241345e35c27b4
unknown <romain.geffrault@gmail.com>
2020-03-24 11:36:38 +0100

    feat: prod configuration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,16 +4,6 @@
 
 export const environment = {
   production: false,
-  firebaseConfig : {
-    apiKey: "AIzaSyAszcew63isTDG_lZoyzVw7wP9NRz2Exwo",
-    authDomain: "sleeping-1a774.firebaseapp.com",
-    databaseURL: "https://sleeping-1a774.firebaseio.com",
-    projectId: "sleeping-1a774",
-    storageBucket: "sleeping-1a774.appspot.com",
-    messagingSenderId: "104196420280",
-    appId: "1:104196420280:web:d089c8bd55a241f74bba4d",
-    measurementId: "G-K8GHT19KPW"
-  },
   serverConfig : {
     serverURL : "http://localhost:80/sleeping-bs/handleRequest.php",
     articlesPath: "http://localhost:80/sleeping-bs/articles/"
