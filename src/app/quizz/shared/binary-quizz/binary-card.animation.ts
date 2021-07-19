e6c5f5c505e83ac40e684a536767621f04658e7e
Romain <romain.geffrault@laposte.net>
2021-07-19 08:49:38 +0200

    feat: improve transition

--- src/app/quizz/shared/binary-quizz/binary-card.animation.ts
+++ src/app/quizz/shared/binary-quizz/binary-card.animation.ts
@@ -5,12 +5,12 @@ trigger('left-right-card', [
 
   transition(":increment", group([
     query(':enter', [
-        style({ transform: 'translateX(-100%) rotate(-45deg)', opacity: '0'}),
-        animate('500ms ease-in-out', style({  transform: 'translateX(0) rotate(0)', opacity: '1' }))
-     ], { optional: true }),
+        style({ opacity: '0', transform: 'scale(80%)'}),
+        animate('500ms ease-in-out', style({  opacity: '1', transform: 'scale(100%)' }))
+     ], { optional: true, delay: 100 }),
     query(':leave', [
       group([
-        animate('500ms ease-in-out', style({  transform: 'translateX(100%) rotate(45deg)', opacity: '0' })),
+        animate('500ms ease-in-out', style({  transform: 'translateX(20%) rotate(10deg)', transformOrigin: 'bottom right',  opacity: '0' })),
         animate('500ms ease-in-out', keyframes([
           style({  background: '#e3fbe4' }),
           style({  background: '#c0f9c2' }),
@@ -23,12 +23,12 @@ trigger('left-right-card', [
 
    transition(":decrement", group([
     query(':enter', [
-        style({ transform: 'translateX(100%) rotate(45deg)', opacity: '0'}),
-        animate('500ms ease-in-out', style({  transform: 'translateX(0) rotate(0)', opacity: '1' }))
-     ], { optional: true }),
+        style({ opacity: '0', transform: 'scale(80%)'}),
+        animate('500ms ease-in-out', style({  opacity: '1', transform: 'scale(100%)' }))
+     ], { optional: true, delay: 100 }),
     query(':leave', [
       group([
-        animate('500ms ease-in-out', style({  transform: 'translateX(-100%) rotate(-45deg)', opacity: '0' })),
+        animate('500ms ease-in-out', style({  transform: 'translateX(-20%) rotate(-10deg)', transformOrigin: 'bottom left',opacity: '0' })),
         animate('500ms ease-in-out', keyframes([
           style({  background: '#f9d8d8' }),
           style({  background: '#f7a0a0' }),
