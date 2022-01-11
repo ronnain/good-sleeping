7a757e86af3f68870835b9044742f2cf25877d7b
Romain <romain.geffrault@laposte.net>
2022-01-11 20:21:17 +0100

    feat: contact form

---
+++ src/app/shared/services/http/contact.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+
+@Injectable({providedIn: 'root'})
+export class ContactService {
+
+    httpOptions = {
+        headers: new HttpHeaders({
+          'Content-Type':  'application/json'
+        })
+      };
+
+    constructor(private http: HttpClient) { }
+
+    sendMessage(firstName: string, email: string, message: string) {
+        const body = {
+            firstName,
+            email,
+            message
+        };
+        const url = environment.serverConfig.serverURL + '?method=sendMessage';
+        return this.http.post<any>(url, body, this.httpOptions);
+    }
+}
