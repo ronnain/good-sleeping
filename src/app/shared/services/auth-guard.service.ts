df9205cc9ef6d0eb0a4f892dee58d5704d29590f
romainGeffrault <romain.geffrault@gmail.com>
2023-09-03 06:44:28 +0200

    upgrade to angular16

--- src/app/shared/services/auth-guard.service.ts
+++ src/app/shared/services/auth-guard.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, Router, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { Router, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuardService implements CanActivate {
+export class AuthGuardService  {
 
   constructor(private authService: AuthService,
     private router: Router) { }
